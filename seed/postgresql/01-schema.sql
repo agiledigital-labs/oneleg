42eee5cbecd4b8ea4d7fa9b2819c00a8232ba336
Sean Dawson <spdawson@agiledigital.com.au>
2020-03-06 14:14:30 +1100

    Add updated_at and created_at to seed data

--- seed/postgresql/01-schema.sql
+++ seed/postgresql/01-schema.sql
@@ -1,5 +1,13 @@
 --
 
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TYPE public.account_type AS ENUM (
     'cheque',
     'savings',
@@ -8,10 +16,12 @@ CREATE TYPE public.account_type AS ENUM (
 
 CREATE TABLE public.customers (
     id SERIAL PRIMARY KEY,
-    first_name TEXT,
+    first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     phone TEXT NOT NULL,
-    email TEXT
+    email TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE public.accounts (
@@ -19,12 +29,31 @@ CREATE TABLE public.accounts (
     name TEXT NOT NULL,
     balance MONEY NOT NULL,
     type public.account_type NOT NULL,
-    customer_id INTEGER NOT NULL REFERENCES public.customers
+    customer_id INTEGER NOT NULL REFERENCES public.customers,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE public.transactions (
     id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     delta MONEY NOT NULL,
-    account_id INTEGER NOT NULL REFERENCES public.accounts
+    account_id INTEGER NOT NULL REFERENCES public.accounts,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE TRIGGER set_timestamp_customers
+BEFORE UPDATE ON public.customers
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp_accounts
+BEFORE UPDATE ON public.accounts
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp_transactions
+BEFORE UPDATE ON public.transactions
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
